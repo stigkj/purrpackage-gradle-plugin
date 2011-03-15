0f56a1526086010d758ad7f8aecfe1d46d080b53
John Roth <please.use@sourceforge.net>
2011-03-15 13:26:22 -0700

    Added copyright info

--- src/main/groovy/net/sourceforge/purrpackage/gradle/PurrPackagePlugin.groovy
+++ src/main/groovy/net/sourceforge/purrpackage/gradle/PurrPackagePlugin.groovy
@@ -1,3 +1,15 @@
+/*
+ * Purrpackage gradle plugin - http://purrpkg-gradle.sourceforge.net
+ * Copyright (C) 2011 John Charles Roth
+ *
+ * This is free software, licensed under the terms of the GNU GPL 
+ * Version 2 or, at your option, any later version. You should have 
+ * received a copy of the license with this file. See the above web address
+ * for more information, or contact the Free Software Foundation, Boston, MA. 
+ * It is distributed WITHOUT WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
 package net.sourceforge.purrpackage.gradle
 
 import org.gradle.api.Plugin;
